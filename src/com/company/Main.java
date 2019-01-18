f8aa9bf016f8edf6310a8c1e50f404615ca1f28c
Nada Mahmoud <nadamahmoud@Nadas-MBP.T-mobile.com>
2019-01-17 22:41:09 -0500

    Start

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println ("Story, and question ");
+
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Choose your next step by entering number 1-4");
+        int option = reader.nextInt();
+
+
+    }
+
+    public static void
+}
