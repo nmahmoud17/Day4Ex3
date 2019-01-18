aeff199cfe7b941cba80a4f22bdc6a3dbb4d0559
Nada Mahmoud <nadamahmoud@Nadas-MacBook-Pro.local>
2019-01-18 08:53:54 -0500

    Add to story

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,16 +6,31 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println ("Story, and question ");
-
-        do {
+        System.out.println ("You're walking to Detroit Labs, and you decided to be productive, you go extra early and nobody is out in the street" +
+                "Going along your merry way, and you suddenly fall into a storm drain. Don't worry though you magically didn't break any bones. You're panicking now though, what do you do next?");
         Scanner reader = new Scanner(System.in);
         System.out.println("What would you like to do? Choose your next step by entering number 1-4");
-        int option = reader.nextInt();
-        if (option = ("1"));
-            choiceOne(int num);
+        String input = reader.next();
 
-    } while (); 
+        while () {
+            String input = in.nextLine();
+            if (input.equals("1")) {
+
+
+            } else if (input.equals("2")) {
+
+            } else if (input.equals("3")) {
+
+            }else if (input.equals ("4"))  {
+
+            } else {
+
+            }
+
+        }
+
+
+    }
 
     public static void choiceOne (int num) {
         if (num==1){
