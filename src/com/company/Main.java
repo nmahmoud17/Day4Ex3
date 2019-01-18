1a5541837ef62033b165f79650ce37ff862c6cec
Nada Mahmoud <nadamahmoud@Nadas-MBP.T-mobile.com>
2019-01-17 22:56:39 -0500

    Outline

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,12 +8,43 @@ public class Main {
 
         System.out.println ("Story, and question ");
 
+        do {
         Scanner reader = new Scanner(System.in);
-        System.out.println("Choose your next step by entering number 1-4");
+        System.out.println("What would you like to do? Choose your next step by entering number 1-4");
         int option = reader.nextInt();
+        if (option = ("1"));
+            choiceOne(int num);
 
+    } while (); 
 
+    public static void choiceOne (int num) {
+        if (num==1){
+            System.out.println("Choice 1");
+        }else {
+
+        }
+    }
+    public static void choiceTwo (int num) {
+        if (num==2){
+            System.out.println("Choice 2");
+        }else {
+
+        }
     }
 
-    public static void
+    public static void choiceThree (int num) {
+        if (num==3){
+            System.out.println("Choice 3");
+        }else {
+
+        }
+    }
+
+    public static void choiceFour (int num) {
+        if (num==3){
+            System.out.println("Choice 4");
+        }else {
+
+        }
+    }
 }
