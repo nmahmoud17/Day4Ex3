622e4110837c00bcf52dcd532ace2da21b7c635d
Nada Mahmoud <nadamahmoud@Nadas-MBP.T-mobile.com>
2019-01-20 19:52:42 -0500

    Pathway 1, for option 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,67 +6,73 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println ("You're walking to Detroit Labs, and you decided to be productive, you go extra early and nobody is out in the street" +
-                "Going along your merry way, and you suddenly fall into a storm drain. Don't worry though you magically didn't break any bones. You're panicking now though, what do you do next?");
-        //code for clicking space for the next prompt
+        boolean playAgain = true;
+        do {
 
-        Scanner reader = new Scanner(System.in);
-        System.out.println("Choose your next step by entering number 1-4"
-        +"1. You decide to scream, hoping someone will rescue you" +
-                " 2. You decide to explore because you have nothing to lose" +
-                "3. You try finding a way to climb out" +
-                " 4. Nothing ");
+            System.out.println ("You're walking to Detroit Labs, and you decided to be productive, you go extra early and nobody is out in the street" +
+                    "Going along your merry way, and you suddenly fall into a storm drain. Don't worry though you magically didn't break any bones. You're panicking now though, what do you do next?");
+            //code for clicking space for the next prompt
 
-        String input = reader.next();
+            Scanner reader = new Scanner(System.in);
+            System.out.println("Choose your next step by entering number 1-4" +
+                    " 1. You decide to scream, hoping someone will rescue you" +
+                    " 2. You decide to explore because you have nothing to lose" +
+                    " 3. You try finding a way to climb out" +
+                    " 4. Nothing ");
 
-        do () {
-            String input = in.nextLine();
-            if (input.equals("1")) {
+            String inputFromUser = reader.next();
+            if (inputFromUser.equals("1")) {
+                System.out.println("Option 1 or Option 2");
 
+                String inputFromUser2 = reader.next();
+                if (inputFromUser2.equals ("Option1")){
+                        System.out.println ("End 1");
+                    }
+                else if (inputFromUser2.equals ("Option2")){
+                    System.out.println ("End 2");
+                }
 
-            } else if (input.equals("2")) {
-
-            } else if (input.equals("3")) {
-
-            }else if (input.equals ("4"))  {
+                else {
+                        System.out.println ("Try Again");
+                        playAgain = false;
+                    }
 
             } else {
-
+                System.out.println ("Make sure you choose one of the options");
             }
 
-        }while ()
+            
 
+    } while (playAgain == true);
 
-    }
-
-    public static void choiceOne (int num) {
-        if (num==1){
-            System.out.println("Nobody ever finds you. Sorry.");
-        }else {
+//    public static void choiceOne (int num) {
+//        if (num==1){
+//            System.out.println("Nobody ever finds you. Sorry.");
+//        }else {
+//
+//        }
+//    }
+//    public static void choiceTwo (int num) {
+//        if (num==2){
+//            System.out.println("You run into the Teenage Mutant Ninja Turtles. You become the newest member, Rembrandt");
+//        }else {
+//
+//        }
+//    }
+//
+//    public static void choiceThree (int num) {
+//        if (num==3){
+//            System.out.println("As your climbing, you slip and pull a lever. It floods the whole place");
+//        }else {
+//
+//        }
+//    }
+//
+//    public static void choiceFour (int num) {
+//        if (num==3){
+//            System.out.println("Surprise! Someone dropped a quarter and saw your body a few months later. You somehow managed to stay alive. ");
+//        }else {
 
         }
     }
-    public static void choiceTwo (int num) {
-        if (num==2){
-            System.out.println("You run into the Teenage Mutant Ninja Turtles. You become the newest member, Rembrandt");
-        }else {
 
-        }
-    }
-
-    public static void choiceThree (int num) {
-        if (num==3){
-            System.out.println("As your climbing, you slip and pull a lever. It floods the whole place");
-        }else {
-
-        }
-    }
-
-    public static void choiceFour (int num) {
-        if (num==3){
-            System.out.println("Surprise! Someone dropped a quarter and saw your body a few months later. You somehow managed to stay alive. ");
-        }else {
-
-        }
-    }
-}
