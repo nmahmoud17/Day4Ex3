7aaf343bef459c84de756c296bef7eab2ebffaba
Nada Mahmoud <nadamahmoud@Nadas-MBP.T-mobile.com>
2019-01-20 22:48:03 -0500

    Editing Option b pathway

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,11 +8,10 @@ public class Main {
     public static void main(String[] args) {
 
 
-
         boolean playAgain = true;
 
         do {
-            System.out.println ("Story");
+            System.out.println("Story");
             //code for clicking space for the next prompt
 
             Scanner reader = new Scanner(System.in);
@@ -21,93 +20,69 @@ public class Main {
                     " 2. You decide to explore because you have nothing to lose" +
                     " 3. You try finding a way to climb out" +
                     " 4. Nothing ");
+
             String inputFromUser = reader.next();
             if (inputFromUser.equals("1")) {
                 System.out.println("Option 1 or Option 1a");
 
-                String inputFromUser2 = reader.next();
-                if (inputFromUser2.equals ("Option1")){
-                        System.out.println ("End 1");
-                        playAgain = true;
-                }
-                else if (inputFromUser2.equals ("Option1a")){
-                    System.out.println ("End 2");
-                        System.out.println ("Would you like to play again?");
-                        String answer = reader.next();
-                            if (answer.equals ("Y")) {
-                                playAgain = true;
-                            }
-                            else {
-                                System.out.println ("Game Over");
-                                playAgain = false;
-                            }
-                }
-
-                else {
-                        System.out.println ("Try Again");
-                        playAgain = false;
-                    }
-
-            }
-
-            if (inputFromUser.equals("2")) {
-                System.out.println("Option 2a or Option 2b");
-
-                String inputFromUser2 = reader.next();
-                if (inputFromUser2.equals("Option2a")) {
-                    System.out.println("End 2a");
+                inputFromUser = reader.next();
+                if (inputFromUser.equals("Option1")) {
+                    System.out.println("End 1");
                     System.out.println("Would you like to play again?");
                     String answer = reader.next();
                     if (answer.equals("Y")) {
                         playAgain = true;
-                    } else if (inputFromUser2.equals("Option2b")) {
-                        System.out.println("End 2b");
-                        System.out.println("Would you like to play again?");
-                        String answer2 = reader.next();
-                        if (answer2.equals("Y")) {
-                            playAgain = true;
-                        } else {
-                            System.out.println("Game Over");
-                            playAgain = false;
-                        }
-
                     }
                 }
-            }
 
-            if (inputFromUser.equals("3")) {
-                System.out.println("Option 3a or Option 3b");
-
-                String inputFromUser3 = reader.next();
-                if (inputFromUser3.equals("Option3a")) {
-                    System.out.println("End 3a");
+                String answer = reader.next();
+                if (inputFromUser.equals("Option1a")) {
+                    System.out.println("End 1a");
                     System.out.println("Would you like to play again?");
-                    String answer3 = reader.next();
-                    if (answer3.equals("Y")) {
+                    answer = reader.next();
+                    if (answer.equals("Y")) {
                         playAgain = true;
                     } else {
-                            System.out.println("Game Over");
-                            playAgain = false;
-                        }
-
+                        System.out.println("Game Over");
+                        playAgain = false;
                     }
-
-                } else if (inputFromUser.equals("Option3b")) {
-                    System.out.println("End 3b");
-                    System.out.println("Would you like to play again?");
-                    String answer4 = reader.next();
-                        if (answer4.equals("Y")) {
-                            playAgain = true;
-                        }else {
-                            System.out.println("Game Over");
-                             playAgain = false;
+                } else {
+                    System.out.println("Try Again");
+                    playAgain = false;
                 }
+
             }
 
-    } while (playAgain == true);
+//            String inputFromUser2a = reader.next();
+//            if (inputFromUser2a.equals("2")) {
+//                System.out.println("Option 2 or Option 2a");
+//
+//                String inputFromUser2b = reader.next();
+//                if (inputFromUser2a.equals("Option2")) {
+//                    System.out.println("End 2a");
+//                    playAgain = true;
+//                } else if (inputFromUser2b.equals("Option2a")) {
+//                    System.out.println("End 2");
+//                    System.out.println("Would you like to play again?");
+//                    String answer = reader.next();
+//                    if (answer.equals("Y")) {
+//                        playAgain = true;
+//                    } else {
+//                        System.out.println("Game Over");
+//                        playAgain = false;
+//                    }
+//                } else {
+//                    System.out.println("Try Again");
+//                    playAgain = false;
+//                }
 
 
-        }
+
+
+        }while(playAgain ==true);
+
+    }
+
 
 //    public static int answer (Scanner reader) {
 //        System.out.println ("Would you like to play again? (1) for Yes (2) for No ");
@@ -139,5 +114,9 @@ public class Main {
 //        }else {
 
         }
+
+
+
+
 
 
