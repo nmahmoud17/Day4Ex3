1bea7cd58ac67a27dc8914cf791be9adba307e97
Nada Mahmoud <nadamahmoud@Nadas-MBP.T-mobile.com>
2019-01-21 20:34:39 -0500

    Option 1 and 1a function

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,17 +4,22 @@ import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
+    Scanner reader = new Scanner(System.in);
+    boolean playAgain;
 
     public static void main(String[] args) {
 
-
+        Scanner reader = new Scanner(System.in);
         boolean playAgain = true;
+
+        Main choiceToPlay;
+        choiceToPlay = new Main();
+        String answer;
+
 
         do {
             System.out.println("Story");
-            //code for clicking space for the next prompt
 
-            Scanner reader = new Scanner(System.in);
             System.out.println("Choose your next step by entering number 1-4" +
                     " 1. You decide to scream, hoping someone will rescue you" +
                     " 2. You decide to explore because you have nothing to lose" +
@@ -24,34 +29,77 @@ public class Main {
             String inputFromUser = reader.next();
             if (inputFromUser.equals("1")) {
                 System.out.println("Option 1 or Option 1a");
+            }
+            else if (inputFromUser.equals ("2")) {
+                System.out.println("Option 2 or Option 2a");
+            }
+            else if (inputFromUser.equals ("3")) {
+                System.out.println("Option 3 or Option 3a");
+            }else if (inputFromUser.equals ("4")) {
+                System.out.println("Option 4 or Option 4a");
+            }else{
+                System.out.println ("Make sure to choose one of the options");
+            }
 
-                inputFromUser = reader.next();
-                if (inputFromUser.equals("Option1")) {
-                    System.out.println("End 1");
-                    System.out.println("Would you like to play again?");
-                    String answer = reader.next();
-                    if (answer.equals("Y")) {
-                        playAgain = true;
-                    }
+            inputFromUser = reader.next();
+            if (inputFromUser.equals("Option1")) {
+                System.out.println("End 1");
+                System.out.println("\n");
+                System.out.println("Would you like to play again?");
+                answer = reader.next();
+                if (answer.equals("Y")) {
+                    System.out.println("Make better choices next time.");
+                    playAgain = true;
                 }
+            } else {
+                System.out.println("Game Over");
+                playAgain = false;
+            }
 
-                String answer = reader.next();
-                if (inputFromUser.equals("Option1a")) {
-                    System.out.println("End 1a");
-                    System.out.println("Would you like to play again?");
-                    answer = reader.next();
-                    if (answer.equals("Y")) {
-                        playAgain = true;
-                    } else {
-                        System.out.println("Game Over");
-                        playAgain = false;
-                    }
+
+            if (inputFromUser.equals("Option1a")) {
+                System.out.println("End 1a");
+                System.out.println("Would you like to play again?");
+                answer = reader.next();
+                if (answer.equals("Y")) {
+                    playAgain = true;
                 } else {
-                    System.out.println("Try Again");
+                    System.out.println("Game Over");
                     playAgain = false;
                 }
-
             }
+
+//            inputFromUser = reader.next();
+//            if (inputFromUser.equals("Option2")) {
+//                System.out.println("End 2");
+//                System.out.println("\n");
+//                System.out.println("Would you like to play again?");
+//                answer = reader.next();
+//                if (answer.equals("Y")) {
+//                    System.out.println("Make better choices next time.");
+//                    playAgain = true;
+//                }
+//            } else {
+//                System.out.println("Game Over");
+//                playAgain = false;
+//            }
+//
+//            inputFromUser = reader.next();
+//            if (inputFromUser.equals("Option2a")) {
+//                System.out.println("End 2a");
+//                System.out.println("\n");
+//                System.out.println("Would you like to play again?");
+//                answer = reader.next();
+//                if (answer.equals("Y")) {
+//                    System.out.println("Make better choices next time.");
+//                    playAgain = true;
+//                }
+//            } else {
+//                System.out.println("Game Over");
+//                playAgain = false;
+//            }
+
+        } while (playAgain);
 
 //            String inputFromUser2a = reader.next();
 //            if (inputFromUser2a.equals("2")) {
@@ -76,44 +124,25 @@ public class Main {
 //                    playAgain = false;
 //                }
 
-
-
-
-        }while(playAgain ==true);
-
-    }
-
-
-//    public static int answer (Scanner reader) {
-//        System.out.println ("Would you like to play again? (1) for Yes (2) for No ");
-//        int answer = reader.nextInt();
-//        if (Objects.equals(answer, "1")) {
-//            return answer;
-//        }
-
-//    }
-//    public static void choiceTwo (int num) {
-//        if (num==2){
-//            System.out.println("You run into the Teenage Mutant Ninja Turtles. You become the newest member, Rembrandt");
-//        }else {
-//
-//        }
-//    }
-//
-//    public static void choiceThree (int num) {
-//        if (num==3){
-//            System.out.println("As your climbing, you slip and pull a lever. It floods the whole place");
-//        }else {
-//
-//        }
-//    }
-//
-//    public static void choiceFour (int num) {
-//        if (num==3){
-//            System.out.println("Surprise! Someone dropped a quarter and saw your body a few months later. You somehow managed to stay alive. ");
-//        }else {
-
         }
+        }
+    
+
+
+
+
+//    public void answer() {
+//        System.out.println("Would you like to play again? (Y) for Yes (N) for No ");
+//
+//        String answer = reader.next();
+//        if (answer.equals("Y")) {
+//            System.out.println("Make better choices this time.");
+//            playAgain = true;
+//        } else {
+//            playAgain = false;
+//        }
+//
+//    }
 
 
 
